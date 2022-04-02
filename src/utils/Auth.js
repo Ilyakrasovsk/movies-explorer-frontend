e70c3a586b1830a2411fd5f24a54f949e8de8c0b
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-02 23:26:49 +0300

    full

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.diplom.ilkras.nomoredomains.work';
+export const BASE_URL = 'http://api.diplom.ilkras.nomoredomains.work';
 
 
 export const registration = ({name, email, password}) => {
@@ -17,7 +17,6 @@ export const registration = ({name, email, password}) => {
 export const authorization = ({email, password}) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
-        credentionals: 'include',
         headers: {
           'Accept': 'application/json',
           "Content-Type": "application/json"
@@ -27,14 +26,13 @@ export const authorization = ({email, password}) => {
         .then(checkResponse)
 }
 
-export const getContent = (jwt) => {
+export const getContent = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
-        credentionals: 'include',
         headers: {
             'Accept': 'application/json',
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${jwt}`
+            "Authorization": `Bearer ${token}`
         }
     })
         .then(checkResponse)
