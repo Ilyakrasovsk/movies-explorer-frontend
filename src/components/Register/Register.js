4e53821715fb8cb4a6b56501237e4035dcbeebd9
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-04 01:31:02 +0300

    Revert "full"
    This reverts commit d3dd8bbf5997f69839b2a6cecc76cc4a58627364.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -26,7 +26,7 @@ function Register(props) {
                     <span className="register__form_span">{errors?.name}</span>
                     <label htmlFor='email' className='register__label'>E-mail</label>
                     <input type='email' id='email' className='register__input'
-                    name="email"
+                    name="name"
                     pattern="^((([0-9A-Za-z]{1}[-0-9A-z\.]{0,30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я\.]{0,30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}\.){1,}[-A-Za-z]{2,})$"
                     onChange={handleChange}
                     required
